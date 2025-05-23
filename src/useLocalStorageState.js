7ea89dc7e92c441ec97b8e9516d02eb0f112275e
Sohail Shaikh <sohail28195@gmail.com>
2025-05-23 15:45:17 +0530

    custom hooks done :zap:

---
+++ src/useLocalStorageState.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export function useLocalStorageState(initialState, key) {
+  const [value, setValue] = useState(function () {
+    const storedValue = localStorage.getItem(key) || "[]";
+    return storedValue ? JSON.parse(storedValue) : initialState;
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  ); // will auto update whenever watched is updated
+
+  return [value, setValue];
+}
